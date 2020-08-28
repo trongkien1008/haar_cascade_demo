13ec22eff39ac551ede9ec6309c648c5fc9b1f7c
Nguyen Trong Kien <trongkien1008@gmail.com>
2020-08-28 14:10:13 +0700

    update

--- haar_cascade_img.py
+++ haar_cascade_img.py
@@ -1,10 +1,10 @@
 import numpy as np
-import cv2 
+import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('demo.jpg')
+img = cv2.imread('demo.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
